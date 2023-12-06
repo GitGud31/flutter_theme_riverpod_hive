f8e41ab6b61217fd7e5c4f0ee1ae48ca9d29d460
4ref <pricehoolo@gmail.com>
2023-12-06 15:33:08 +0100

    ui styling

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,12 +1,20 @@
-import 'package:dummy_app/controllers/theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'controllers/theme_controller.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  TextStyle _getTextStyle(String mode) {
+    final textColor = (mode == 'dark') ? Colors.white : Colors.black;
+
+    return TextStyle(fontSize: 20, color: textColor);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final themeNotifier = ref.read(themeController.notifier);
     final mode = ref.watch(themeController).theme;
 
     return Scaffold(
@@ -14,25 +22,48 @@ class HomeScreen extends ConsumerWidget {
         title: const Text('Riverpod - Hive'),
         actions: [
           Switch(
-            value: (mode == 'dark') ? true : false,
-            onChanged: (value) =>
-                ref.read(themeController.notifier).toggle(value),
+            value: mode == 'dark',
+            onChanged: (value) => themeNotifier.toggle(value),
           ),
         ],
       ),
       body: Center(
-          child: RichText(
-        text: TextSpan(
-          text: 'Current theme mode ',
-          style: const TextStyle(fontSize: 20),
-          children: <TextSpan>[
-            TextSpan(
-              text: mode,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const ThemedIcon(),
+            RichText(
+              text: TextSpan(
+                text: 'Current theme mode ',
+                style: _getTextStyle(mode),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: mode,
+                    style: _getTextStyle(mode).copyWith(
+                        decoration: TextDecoration.underline,
+                        fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      )),
+      ),
     );
+  }
+}
+
+class ThemedIcon extends ConsumerWidget {
+  const ThemedIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mode = ref.watch(themeController).theme;
+
+    final iconColor = (mode == 'dark') ? Colors.grey : Colors.orange;
+    final iconData = (mode == 'dark') ? Icons.dark_mode : Icons.light_mode;
+
+    return Icon(iconData, color: iconColor, size: 50);
   }
 }
